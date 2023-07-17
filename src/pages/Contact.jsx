15648abfd711a15a505c47703fe4c1003aca6718
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-07-17 22:35:16 +0100

    email validation added

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,27 +1,28 @@
 import React, { useState } from "react";
 
 const Contact = () => {
-  const [formState, setFromState] = useState({
-    name: "",
-    email: "",
-    message: "",
-  });
-  const [emailError, setEmailError] = useState('')
+
+  const [email, setEmail] = useState('');
+  const [emailError, setEmailError] = useState('');
+
+  function validEmail(email) {
+    return /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+
+  }
+
+  const handleEmail = (e) => {
+    if (!validEmail(e.target.value)) {
+      setEmailError('Email is invalid');
+    } else {
+      setEmailError('')
+    }
+    setEmail(e.target.value)
+  }
 
   const handleBlank = (e) => {
-    console.log(e.target.value);
-
     if (e.target.value === "") {
       alert(`${e.target.name} Required`);
-    } else if (e.target.id === "email") {
-        
-    } else {
-    }
-
-    const handleSubmit = () => {
-        console.log(this)
-    }
-    
+    } 
   };
 
   return (
@@ -42,12 +43,14 @@ const Contact = () => {
           ></input>
           <label htmlFor="email">Email Address</label>
           <input
-            type="text"
+            type="email"
             className="w-5/6 p-1 text-black"
             id="email"
             name="Email"
+            value={email}
             placeholder="Email Address"
             onBlur={handleBlank}
+            onChange={handleEmail}
           ></input>
           <label htmlFor="message">Message</label>
           <textarea
@@ -57,6 +60,7 @@ const Contact = () => {
             name="Message"
             onBlur={handleBlank}
           ></textarea>
+          {emailError && <h2 className="" style={{color: 'red'}}>{emailError}</h2>}
           <button className="px-2 py-1 m-2 bg-slate-300 custom-color self-end border-2 border-black rounded-lg" >
             Submit
           </button>
