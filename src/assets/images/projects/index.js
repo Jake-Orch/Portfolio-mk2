83a76c7c771692e4a17d2d56b8c5f2993387e30a
Aurelian <123247355+Jake-Orch@users.noreply.github.com>
2023-07-17 13:20:56 +0100

    image issue

---
+++ src/assets/images/projects/index.js
@@ -0,0 +1,19 @@
+import NoteMakerPWA from './NoteMakerPWA.png';
+import PortfolioMk2 from './PortfolioMk2.png';
+import SafeKeeping from './SafeKeeping.png';
+import WeatherDashboard from './WeatherDashboard.png';
+import WeatherIGo from './WeatherIGo.png';
+import WizDumb from './WizDumb.png'
+
+const img = [NoteMakerPWA, PortfolioMk2, SafeKeeping, WeatherDashboard, WeatherIGo, WizDumb]
+
+const IMAGES = {
+    NoteMakerPWA: require('./NoteMakerPWA.png').default,
+    PortfolioMk2: require('./PortfolioMk2.png').default,
+    SafeKeeping: require('./SafeKeeping.png').default,
+    WeatherDashboard: require('./WeatherDashboard.png').default,
+    WeatherIGo: require('./WeatherIGo.png').default,
+    WizDumb: require('./WizDumb.png').default
+}
+
+export default img;
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,68 @@
+import Project from "../components/Project";
+
 const Portfolio = () => {
-    return (
-        <h1>Portfolio Page</h1>
-    )
-}
+  const projects = [
+    {
+      name: "WizDumb",
+      img: "WizDumb",
+      alt: "WizDumb Homepage",
+      deployed: "/",
+      git: "https://github.com/Cdrcar/Wizdumb",
+    },
+    {
+      name: "Safe Keeping",
+      img: "SafeKeeping",
+      alt: "Safe Keeping Homepage",
+      deployed: "https://safekeeping.herokuapp.com/",
+      git: "https://github.com/Jake-Orch/Project2",
+    },
+    {
+      name: "Weather I Go",
+      img: "WeatherIGo",
+      alt: "Weather I Go Homepage",
+      deployed: "https://faithscoding.github.io/weather-i-go/",
+      git: "https://github.com/FaithsCoding/weather-i-go",
+    },
+    {
+      name: "Note Maker PWA",
+      img: "NoteMakerPWA",
+      alt: "Note Maker PWA Homepage",
+      deployed: "https://note-maker-pwa-48cf86482d7b.herokuapp.com/",
+      git: "https://github.com/Jake-Orch/myNoteMakerPWA",
+    },
+    {
+      name: "Weather Dashboard",
+      img: "WeatherDashboard",
+      alt: "Weather Dashboard Homepage",
+      deployed: "https://jake-orch.github.io/Weather-dashboard/",
+      git: "https://github.com/Jake-Orch/Weather-dashboard",
+    },
+    {
+      name: "Portfolio Mk2",
+      img: "PortfolioMk2",
+      alt: "Portfolio Mk2 Homepage",
+      deployed: "/",
+      git: "https://github.com/Jake-Orch/Portfolio-mk2",
+    },
+  ];
+
+  return (
+    <div>
+      <h1 className="grid justify-items-center mt-10 custom-color text-bold">Portfolio Page</h1>
+      <div className="grid grid-cols-3 justify-items-center my-14">
+        {projects.map((project) => (
+          <Project
+            name={project.name}
+            key={project.name}
+            img={project.img}
+            alt={project.alt}
+            deployed={project.deployed}
+            git={project.git}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
 
 export default Portfolio;
